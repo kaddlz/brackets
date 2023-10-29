c1c2f39424d1bf28da07d77c5acd772d1f97528a
kaddlz <roslanov@icloud.com>
2023-10-30 01:43:00 +0300

    feat: complete brackets taks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let map = new Map(bracketsConfig);
+  for (let char of str) {
+    if (stack.length === 0 || map.has(char)) {
+      if (map.get(char) === char) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else {
+      let top = stack.pop();
+      if (map.get(top) !== char) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
